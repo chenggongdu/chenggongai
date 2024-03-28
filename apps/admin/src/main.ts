353a87205410bff8e6749896d21175937f9c80a4
杜成功 <duchenggong@jiaoyu361.com>
2024-03-28 10:52:46 +0800

    feat(project-init): add ant-design-vue and unocss to admin workspace

--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -5,10 +5,14 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+import 'virtual:uno.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Antd)
 
 app.mount('#app')
--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -4,14 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    VueDevTools(),
-  ],
+  plugins: [vue(), vueJsx(), VueDevTools(), UnoCSS()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

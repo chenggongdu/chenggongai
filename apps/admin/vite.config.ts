cb334b4e9cace1f6c35f4a83f47e8771d57d3726
杜成功 <duchenggong@jiaoyu361.com>
2024-03-29 09:07:05 +0800

    feat(alias): add config for  project directory

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,17 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
+import path from 'path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
 import UnoCSS from 'unocss/vite'
-
+const resolve = (dir: string): string => path.resolve(__dirname, dir)
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), VueDevTools(), UnoCSS()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': resolve('src'),
+      '#': resolve('types'),
+      '@hooks': resolve('src/hooks'),
+      '@cp': resolve('src/components'),
+      '@views': resolve('src/views')
     }
   }
 })
